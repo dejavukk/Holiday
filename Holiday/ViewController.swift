fb2cadbb0184ce7aebf44f117d3626aba4b83ad1
junhyuk <dejavukk1@gmail.com>
2019-10-18 23:41:33 +0900

    내비게이션 추가, SearchBar추가 오토레이아웃.

--- Holiday/ViewController.swift
+++ Holiday/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 00ce2a63cd85075c0d1ff7d589d272570d584821 - API Key.
+    // https://calendarific.com/api/v2
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var searchBar: UISearchBar!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+
+extension ViewController: UITableViewDelegate {
+    
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+}
